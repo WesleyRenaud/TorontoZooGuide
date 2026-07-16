5dcbc22e24c281f18179d32463110fbb0f7c372b
Wesley Renaud <wesleyrenaud736@gmail.com>
2026-07-15 21:16:55 -0400

    Fixed.

--- api/itinerary/operations/commit_itinerary_item_schedule_change.py
+++ api/itinerary/operations/commit_itinerary_item_schedule_change.py
@@ -9,7 +9,9 @@ from ..domain.itinerary import build_current_itinerary
 from ..domain.itinerary_adjustment import ItineraryAdjustment
 from ...guardians.coordinators.guardians_coordinator import GuardiansCoordinator
 from ..guardians_talk_item_key import GuardiansTalkScheduleItemKey
+from ..results.itinerary_save_result import ItinerarySaveResult
 from ..scheduling.bulk.guardians_talk_covered_animals import restore_covered_animals_after_talk_removed
+from ..scheduling.core.time_block import latest_scheduled_end_seconds
 from ..scheduling.items.schedule_item_key import ScheduleItemKey
 from ..scheduling.items.schedule_itinerary_helpers import build_itinerary_context
 from ..scheduling.items.schedule_itinerary_helpers import build_success_result
@@ -21,6 +23,7 @@ from ..scheduling.unscheduling.update_visit_times_after_schedule_item_removed im
 from ..scheduling.unscheduling.update_visit_times_after_schedule_item_removed import update_departure_to_latest_scheduled_end
 from ..scheduling.unscheduling.update_visit_times_after_schedule_item_removed import was_first_scheduled_item
 from ..scheduling.unscheduling.update_visit_times_after_schedule_item_removed import was_last_scheduled_item
+from ...shared.calendar_dates import DateValues
 from ...types import Connection
 from ...types import Cursor
 from ...wild_encounters.coordinators.wild_encounter_coordinator import WildEncounterCoordinator
@@ -53,6 +56,13 @@ def commit_itinerary_item_schedule_change(
    removed_last_item = was_last_scheduled_item(
       itinerary_before,
       removed_block )
+   previous_latest_end_seconds = latest_scheduled_end_seconds( itinerary_before )
+   previous_departure_seconds = DateValues.time_value_in_seconds(
+      itinerary_before.departure_time )
+   departure_pinned_to_latest = (
+      previous_latest_end_seconds is not None
+      and previous_departure_seconds is not None
+      and previous_departure_seconds == previous_latest_end_seconds )
    restored_talk_covered_animals = None
    cur = conn.cursor()
 
@@ -108,7 +118,20 @@ def commit_itinerary_item_schedule_change(
       if arrival_adjustment is not None:
          adjustments.append( arrival_adjustment )
 
-   if removed_last_item:
+   should_update_departure = removed_last_item
+
+   if (
+         not should_update_departure
+         and departure_pinned_to_latest
+         and previous_latest_end_seconds is not None ):
+      latest_end_after = latest_scheduled_end_seconds( itinerary_after )
+
+      if (
+            latest_end_after is not None
+            and latest_end_after < previous_latest_end_seconds ):
+         should_update_departure = True
+
+   if should_update_departure:
       departure_adjustment = update_departure_to_latest_scheduled_end(
          conn,
          itinerary_after,
